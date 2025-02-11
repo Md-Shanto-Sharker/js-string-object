322f88fedc5dea531ef6d9d43975d585c2e8f93d
SHANTO <rabbymd55555@gmail.com>
2025-02-11 23:33:07 +0600

    added reverse.js file

--- reverse.js
+++ reverse.js
@@ -1,5 +1,4 @@
-const sentence = 'I am learning Web Dev.'
-
+// const sentence = 'I am learning Web Dev.'
 // let reverse = '';
 // for(const letter of sentence){
 //     reverse= letter + reverse
@@ -7,14 +6,43 @@ const sentence = 'I am learning Web Dev.'
 // }
 // console.log(reverse);
 
+// const name = 'shanto';
+// let reverse = '';
+// for (n of name){
+//     reverse = n + reverse
+//     // console.log(reverse);
+// }
+// console.log(reverse);
 
+// const sentence = "I am learning Web Dev.";
+// let rev = ''
+// for (let i = 0; i < sentence.length; i++) {
+//   let letter = sentence[i];
+//   rev = letter + rev;
+// }
+// console.log(rev);
 
+// const name = "javascript";
+// let reverse = "";
+// for (let n = 0; n < name.length; n++) {
+//     const letter = name[n];
+//     reverse= letter + reverse;
+// }
+// console.log(reverse);
 
-const name = 'shanto';
+// Use of while loop
 
-let reverse = '';
-for (n of name){
-    reverse = n + reverse
-    // console.log(reverse);
-}
+// const name = "javascript";
+// let n = 0;
+// let reverse = '';
+// while(n<name.length){
+//     const letter = name[n];
+//     reverse = letter + reverse;
+//     n++
+// }
+// console.log(reverse);
+
+// use of method>>>>>>>>>>>>>>>>
+const name = 'javascript';
+const reverse = name.split('').reverse().join('');
 console.log(reverse);
