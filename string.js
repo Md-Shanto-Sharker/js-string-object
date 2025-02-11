0f74c9de571a5fa240f39b3b8957ba4f33ed41b9
SHANTO <rabbymd55555@gmail.com>
2025-02-11 19:44:20 +0600

    added string.js file

--- string.js
+++ string.js
@@ -15,4 +15,27 @@ const thana = new String('Gazipur');
 // console.log(typeof thana);
 
 
-const numbers =[54,98,78,21,65];
+// const numbers =[54,98,78,21,65];
+// console.log(numbers.length);
+// console.log(numbers[3]);
+
+
+// const capital ='Bangladesh';
+// console.log(capital[2]);
+// console.log(capital);
+// console.log(capital.length);
+
+
+
+// const numbers =[54,98,78,21,65];   //array is mutable>>Not changeable
+// console.log(numbers);
+// numbers[1]=10;
+// console.log(numbers);
+
+
+
+
+const capital ='Bangladesh'; //string is immutable>>changeable
+console.log(capital);
+capital[1]='a'     
+console.log(capital);  //no change in string 
