4dcad0a14466a91d34a3bb4258f18476b62b27ab
SHANTO <rabbymd55555@gmail.com>
2025-02-11 21:38:22 +0600

    added slice.js file

---
+++ slice.js
@@ -0,0 +1,17 @@
+// Use of slice() function>>>>>>>>>
+
+// const address = 'andorkilla';
+// const part = address.slice(2,5);
+// console.log(part);
+
+
+// use of split() function>>>>>>>>>>>>>>>>>>>>
+
+// const sentence = 'I am a good and  hardworking person';
+// console.log(sentence.split(' '));
+// console.log(sentence.split('a'));
+
+
+const friends = 'shanto,sharker,irfan,rasel';
+
+console.log(friends.split(','));
