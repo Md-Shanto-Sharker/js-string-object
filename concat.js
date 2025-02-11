aeb3ae19a5122037b8a9f5fc6ab3988f887307d2
SHANTO <rabbymd55555@gmail.com>
2025-02-11 21:57:54 +0600

    added concat.js file

--- concat.js
+++ concat.js
@@ -4,9 +4,18 @@
 // console.log(first.concat(mid).concat(second));
 // console.log(first.concat(' ').concat(mid).concat(' ').concat(second));
 
-const first = "md";
-const mid = "shanto";
-const second = "sharker";
 
-console.log(first + mid + second);
-console.log(first + " " + mid + " " + second);
+
+
+// const first = "md";
+// const mid = "shanto";
+// const second = "sharker";
+
+// console.log(first + mid + second);
+// console.log(first + " " + mid + " " + second);
+
+
+
+// use of includes>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const name = 'shanto';
+console.log(name.includes('a'));
