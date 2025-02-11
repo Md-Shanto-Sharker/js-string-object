7b80b992c67472a128d01496b74b1ce3225632f3
SHANTO <rabbymd55555@gmail.com>
2025-02-11 11:21:58 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,18 @@
+const country ='Bangladesh';
+const division ="NewKhale";
+const district =`B-Baria`;
+const thana = new String('Gazipur');
+// console.log(country);
+// console.log(typeof country);
+
+// console.log(division);
+// console.log(typeof division);
+
+// console.log(district);
+// console.log(typeof district);
+
+// console.log(thana);
+// console.log(typeof thana);
+
+
+const numbers =[54,98,78,21,65];
