8565ba6e28230cf031c869982087e450aa338a74
SHANTO <rabbymd55555@gmail.com>
2025-02-11 22:36:40 +0600

    added problem-1.js file

--- problem-1.js
+++ problem-1.js
@@ -1,3 +1,8 @@
 // তোমাকে একটি নাম দেওয়া হবে, সেটাকে উল্টিয়ে প্রিন্ট করতে হবে।
 
 let name = "JavaScript";
+let reverse = '';
+for ( let n of name){
+    reverse= n + reverse;
+}
+console.log(reverse);
