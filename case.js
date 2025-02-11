e8d3d458baa507344e0d2f74369a9e68da70a850
SHANTO <rabbymd55555@gmail.com>
2025-02-11 21:19:29 +0600

    added case..js file

---
+++ case.js
@@ -0,0 +1,29 @@
+
+// Use of toUpperCase()  or toLowerCase() function>>>>>>
+
+// const collage = "Model Institute of science and technology"
+// console.log(collage.toUpperCase());
+// console.log(collage.toLowerCase());
+
+// const subject = "Chemistry";
+// const book = "chemistry";
+
+// if (subject.toUpperCase() === book.toUpperCase()) {
+//   console.log("I am reading book ");
+// } else {
+//   console.log("I can't reading book");
+// }
+
+
+
+
+// Use of trim() function >>>>>>>>>>>>>>>>>>>>>
+
+const drink =' water ';
+const liquid ='water';
+
+if(drink.trim() === liquid.trim()){
+    console.log("same value");
+}else{
+    console.log("are not same");
+}
