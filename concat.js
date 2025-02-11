ae1bf04f6bc89b089ee9c8605a9faa063b24bfdd
SHANTO <rabbymd55555@gmail.com>
2025-02-11 21:55:37 +0600

    added concat.js file

---
+++ concat.js
@@ -0,0 +1,12 @@
+// const first = 'md';
+// const mid = 'shanto'
+// const second = 'sharker'
+// console.log(first.concat(mid).concat(second));
+// console.log(first.concat(' ').concat(mid).concat(' ').concat(second));
+
+const first = "md";
+const mid = "shanto";
+const second = "sharker";
+
+console.log(first + mid + second);
+console.log(first + " " + mid + " " + second);
