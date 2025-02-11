fb08196f3811731a7c61efea25ca45d2b793fbf3
SHANTO <rabbymd55555@gmail.com>
2025-02-11 22:30:38 +0600

    added reverse.js file

---
+++ reverse.js
@@ -0,0 +1,20 @@
+const sentence = 'I am learning Web Dev.'
+
+// let reverse = '';
+// for(const letter of sentence){
+//     reverse= letter + reverse
+//     console.log(reverse);
+// }
+// console.log(reverse);
+
+
+
+
+const name = 'shanto';
+
+let reverse = '';
+for (n of name){
+    reverse = n + reverse
+    // console.log(reverse);
+}
+console.log(reverse);
