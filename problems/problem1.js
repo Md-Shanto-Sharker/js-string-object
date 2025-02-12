fea0f54218d752bdb206bbb5aab5cb4269b9ed61
SHANTO <rabbymd55555@gmail.com>
2025-02-12 17:55:02 +0600

    added problem1.js file

---
+++ problems/problem1.js
@@ -0,0 +1,11 @@
+// Task-1:
+// Count how many times a string has the letter a
+
+let count = 0;
+const string = 'shanto';
+for(let i of string){
+    if(i==='a'){
+        count++
+    }
+}
+console.log(count);
