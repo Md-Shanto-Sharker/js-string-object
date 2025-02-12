80ddf285ddf3825e677f3d5771b5d4f689039079
SHANTO <rabbymd55555@gmail.com>
2025-02-12 15:51:00 +0600

    added loop.js file

---
+++ loop.js
@@ -0,0 +1,13 @@
+const mobile = {
+    brand : 'samsung',
+    price : 25000,
+    color : 'black',
+    camera :'12mp',
+    isNew : true
+}
+for(const a in mobile){
+    console.log(a);
+    console.log(mobile[a]);
+    
+}
+
