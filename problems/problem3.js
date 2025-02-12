672954683765a92e783d0e630783d181dc50ba90
SHANTO <rabbymd55555@gmail.com>
2025-02-12 19:07:48 +0600

    added problem3.js file

---
+++ problems/problem3.js
@@ -0,0 +1,12 @@
+// Task-3:
+// Check whether a string contains all the vowels a, e, i, o, u
+
+const string = 'aeiou';
+if(string.includes('a')&& string.includes('e')&& string.includes('i')&&string.includes('o')&& string.includes('u')){
+    console.log('yes here');
+}
+else{
+    console.log('No');
+}
+
+
