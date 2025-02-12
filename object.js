b46b610a3158add40c89b99969c47be841b9d00a
SHANTO <rabbymd55555@gmail.com>
2025-02-12 11:23:12 +0600

    added object.js file

---
+++ object.js
@@ -0,0 +1,21 @@
+// const bottle = {
+//   brand: "apple",
+//   price: 45,
+//   color: "white",
+//   isClean: false,
+// };
+// console.log(bottle);
+
+
+const bottle = {
+    brand: "apple",
+    price: 45,
+    color: ['white','black','red','blue'],
+    subject:{
+        name:'bangla',
+        marks : 100,
+        exam : 'yes'
+
+    }
+  };
+  console.log(bottle);
