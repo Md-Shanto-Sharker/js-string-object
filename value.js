6408acb4aa2d05ea38e312fc2c5e85eb2ca973d8
SHANTO <rabbymd55555@gmail.com>
2025-02-12 11:49:19 +0600

    added value.js file

---
+++ value.js
@@ -0,0 +1,16 @@
+const person = {
+    name : 'soder uddin',
+    age : 25,
+    profession : 'developer',
+    salary : 25000,
+    married : true,
+}
+
+// change the value>>>>>>>>>>>>>
+//use the dot notation>>>>>>>>>>>>
+person.name='shanto';
+console.log(person);
+
+//  use bracket notation>>>>>>>
+person['salary']= 50000;
+console.log(person);
