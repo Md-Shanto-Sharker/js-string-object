119c580d08b7dfc58976746ae86b153d68489dec
SHANTO <rabbymd55555@gmail.com>
2025-02-12 11:41:23 +0600

    added property.js file

---
+++ property.js
@@ -0,0 +1,18 @@
+const person = {
+    name : 'soder uddin',
+    age : 25,
+    profession : 'developer',
+    salary : 25000,
+    married : true,
+}
+// console.log(person);
+// Use of dot notation....
+// console.log(person.name);
+// const income = person.salary;
+// console.log(income);
+
+
+// bracket notation >>>>>>>>>>>>>>
+console.log(person['profession']);
+const status = person['married'];
+console.log(status);
